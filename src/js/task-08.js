b647a74972845b3688fd4f6795e8f9f7d9feb518
Svitlana Ilnytska <svitlanailnicka@gmail.com>
2021-09-13 23:56:43 +0300

    add eight task

---
+++ src/js/task-08.js
@@ -0,0 +1,36 @@
+const refs = {
+  createBtn: document.querySelector('[data-action="render"]'),
+  removeBtn: document.querySelector('[data-action="destroy"]'),
+  square: document.querySelector("#boxes"),
+  container: document.querySelector("#controls"),
+};
+
+const createBoxes = (amount) => {
+  let basicSquare = 30;
+  let box = document.createDocumentFragment();
+
+  for (let i = 0; i < amount; i += 1) {
+    let sizeNextSquare = basicSquare + i * 10;
+
+    const div = document.createElement("div");
+    div.style.width = `${sizeNextSquare}px`;
+    div.style.height = `${sizeNextSquare}px`;
+    div.style.backgroundColor = `rgba( ${randomColor()} , ${randomColor()} , ${randomColor()} )`;
+    box.append(div);
+  }
+  boxes.append(box);
+};
+const randomColor = () => {
+  return Math.floor(Math.random() * 256);
+};
+
+function getAmount() {
+  let amount = document.querySelector("#controls input").value;
+  createBoxes(amount);
+}
+const destroyBoxes = () => {
+  refs.square.innerHTML = "";
+};
+
+refs.createBtn.addEventListener("click", getAmount);
+refs.removeBtn.addEventListener("click", destroyBoxes);
