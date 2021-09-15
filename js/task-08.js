320dd0851a8bb873d44f4dda53d28ed0311e39b3
Svitlana-Ilnytska <72188793+Svitlana-Ilnytska@users.noreply.github.com>
2021-09-15 20:17:31 +0300

    fixed eight task

--- js/task-08.js
+++ js/task-08.js
@@ -1,34 +1,36 @@
+let squareSize = 30;
 const refs = {
   createBtn: document.querySelector('[data-action="render"]'),
   removeBtn: document.querySelector('[data-action="destroy"]'),
   square: document.querySelector("#boxes"),
-  // container: document.querySelector("#controls"),
+  input: document.querySelector("#controls input"),
 };
 
 const createBoxes = (amount) => {
-  const basicSquare = 30;
+
   const box = document.createDocumentFragment();
 
-  for (let i = 0; i < amount; i += 1) {
-    const sizeNextSquare = basicSquare + i * 10;
-
+  for (let i = 1; i <= amount; i += 1) {
     const div = document.createElement("div");
-    div.style.width = `${sizeNextSquare}px`;
-    div.style.height = `${sizeNextSquare}px`;
+    div.style.width = `${squareSize}px`;
+    div.style.height = `${squareSize}px`;
     div.style.backgroundColor = `rgba( ${randomColor()} , ${randomColor()} , ${randomColor()} )`;
     box.append(div);
+    squareSize = squareSize + 10;
   }
   boxes.append(box);
+  refs.input.value = "";
 };
+
 const randomColor = () => {
   return Math.floor(Math.random() * 256);
 };
 
 function getAmount() {
-  let amount = document.querySelector("#controls input").value;
-  createBoxes(amount);
+  createBoxes(refs.input.value);
 }
 const destroyBoxes = () => {
+  squareSize = 30;
   refs.square.innerHTML = "";
 };
 
